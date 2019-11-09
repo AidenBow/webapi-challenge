761203af785abcc8e6079d612023ed9047013d47
AidenBow <AidenBow@users.noreply.github.com>
2019-11-08 18:58:00 -0800

    verifyPost and verifyPostId middleware

--- data/routes/projectRoute.js
+++ data/routes/projectRoute.js
@@ -3,12 +3,13 @@ const express = require('express');
 const router = express.Router();
 const projectDb = require('../helpers/projectModel')
 
-router.post('/', (req, res) => {
+router.post('/', verifyPost, (req, res) => {
   projectDb.insert(req.body)
     .then(project => {
       res.status(200).json(project)
     })
     .catch(err => {
+      console.log(err)
       res.status(500).json({message: "server error"})
     })
 })
@@ -19,38 +20,63 @@ router.get('/', (req, res) => {
       res.status(200).json(projects)
     })
     .catch(err => {
+      console.log(err)
       res.status(500).json({message: "server error"})
     })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', verifyPostId, (req, res) => {
   projectDb.get(req.params.id)
     .then(project => {
       res.status(200).json(project)
     })
     .catch(err => {
+      console.log(err)
       res.status(500).json({message: "server error"})
     })
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', verifyPostId, (req, res) => {
   projectDb.update(req.params.id, req.body)
     .then(project => {
       res.status(200).json(project)
     })
     .catch(err => {
+      console.log(err)
       res.status(500).json({message: "server error"})
     })
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', verifyPostId, (req, res) => {
   projectDb.remove(req.params.id)
     .then(project => {
       res.status(200).json({message: `project ${req.params.id} has been deleted`})
     })
     .catch(err => {
+      console.log(err)
       res.status(500).json({message: "server error"})
     })
 })
+
+//@@@@@@@@@@@@@@@@ MIDDLEWARE @@@@@@@@@@@@@@@@@@
+
+function verifyPost(req, res, next) {
+  if (!req.body.name || !req.body.description) {
+    res.status(404).json({message: "you are missing either, or both of: name and description"})
+  } else {
+    next()
+  }
+}
+
+function verifyPostId(req, res, next) {
+  projectDb.get(req.params.id)
+    .then(post => {
+      if (post) {
+        next();
+      } else {
+        res.status(404).json({message: `post ${req.params.id} does not exist `})
+      }
+    })
+}
 
 module.exports = router;
