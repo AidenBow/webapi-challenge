6cdbbc67eb9c3eb9563d216a994dee98bfe80934
AidenBow <AidenBow@users.noreply.github.com>
2019-11-08 19:21:21 -0800

    actionRoute base

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const server = express();
 
 const projectRouter = require("./data/routes/projectRoute")
+const actionRouter = require("./data/routes/actionRoute")
 
 server.use(express.json());
 server.use("/projects", projectRouter)
+server.use("/actions", actionRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h1>This is Aiden API!</h1>`)
